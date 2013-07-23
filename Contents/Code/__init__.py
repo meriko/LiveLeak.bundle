15cd293c3f3c6fea108ea89aebea6ea6d528b81a
Erik Olsson <meriko@hotmail.com>
2013-07-23 10:17:26 +0200

    Added predefined categories. Added support for Safe Mode browsing. Fixed descriptions for videos

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -3,11 +3,40 @@ ART    = 'art-default.jpg'
 ICON   = 'icon-default.png'
 PREFIX = '/video/liveleak'
 
-BASE_URL = "http://www.liveleak.com"
-
-RE_VIDEO_URL = Regex('.*file *: *\"(.*\.mp4.*)\".*', Regex.IGNORECASE)
+BASE_URL = "http://www.liveleak.com/"
 
 HTTP_USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/536.30.1 (KHTML, like Gecko) Version/6.0.5 Safari/536.30.1"
+
+PREDEFINED_CATEGORIES = [ 
+	{
+		'title':	'Popular Recent Items',
+		'url':		BASE_URL + 'browse?selection=popular'
+	},
+	{
+		'title':	'All Recent Items',
+		'url':		BASE_URL + 'browse?selection=all'
+	},
+	{
+		'title':	'Upcoming Items',
+		'url':		BASE_URL + 'browse?upcoming=1'
+	},
+	{
+		'title':	'Top Items Today',
+		'url':		BASE_URL + 'browse?rank_by=day'
+	},
+	{
+		'title':	'Top Items Week',
+		'url':		BASE_URL + 'browse?rank_by=week'
+	},
+	{
+		'title':	'Top Items Month',
+		'url':		BASE_URL + 'browse?rank_by=month'
+	},
+	{
+		'title':	'Top Items All Time',
+		'url':		BASE_URL + 'browse?rank_by=all_time'
+	},
+]
 
 ##########################################################################################
 def Start():
@@ -18,15 +47,36 @@ def Start():
 
 	HTTP.CacheTime             = CACHE_1HOUR
 	HTTP.Headers['User-agent'] = HTTP_USER_AGENT
-	
+
+##########################################################################################
+def ValidatePrefs():
+	oc = ObjectContainer()
+	oc.header  = "Note!"
+	oc.message = "Please restart channel for changes to take affect"
+	return oc
+
 ##########################################################################################
 @handler(PREFIX, TITLE, thumb = ICON, art = ART)
 def MainMenu():
 	oc = ObjectContainer()
 	
-	pageElement = HTML.ElementFromURL(BASE_URL)
+	pageElement = HTML.ElementFromURL(CreateURL(BASE_URL))
 	
-	# Add all channels
+	# Add predefined categories
+	for category in PREDEFINED_CATEGORIES:
+		oc.add(
+			DirectoryObject(
+				key = 
+					Callback(
+						Videos, 
+						name = category['title'], 
+						url = CreateURL(category['url'])
+					), 
+				title = category['title']
+		)
+	)
+	
+	# Add categories parsed from site
 	for item in pageElement.xpath("//*[@id = 'subnav']//li"):
 		url = item.xpath(".//a/@href")[0]
 		
@@ -39,28 +89,43 @@ def MainMenu():
 						Callback(
 							Videos, 
 							name = title, 
-							url = BASE_URL + "/" + url,
-							page = 1
+							url = CreateURL(BASE_URL + url)
 						), 
 					title = title
 				)
 			)		
 	
+	# Add preference for Safe Mode
+	oc.add(PrefsObject(title = "Preferences..."))
+	
 	return oc
 	
 ##########################################################################################
 @route(PREFIX + "/Videos", page = int)
-def Videos(name, url, page):
+def Videos(name, url, page = 1):
 	oc = ObjectContainer(title1 = name)
 
-	pageElement = HTML.ElementFromURL(url + '#item_page=' + str(page))
+	pageElement = HTML.ElementFromURL(CreateURL(url + '#item_page=' + str(page)))
 	
 	for item in pageElement.xpath("//*[@class = 'item_list']//li"):
 		try:
 			link    = item.xpath(".//a/@href")[0]
+			
+			if not link.startswith("http://www.liveleak.com/view"):
+				continue
+				
 			title   = item.xpath(".//a/text()")[0]
-			summary = item.xpath(".//div//text()")[0]
-			thumb   = item.xpath(".//a//img/@src")[0]
+			
+			try:
+				Log(item.xpath(".//div/text()"))
+				summary = item.xpath(".//div/text()")[4].strip()
+			except:
+				summary = None
+				
+			try:
+				thumb = item.xpath(".//a//img/@src")[0]
+			except:
+				thumbe = None
 			
 			oc.add(
 				VideoClipObject(
@@ -89,3 +154,17 @@ def Videos(name, url, page):
 	)
 
 	return oc
+
+##########################################################################################
+def CreateURL(url):
+	if not '?' in url:
+		url = url + '?'
+	else:
+		url = url + '&'
+		
+	if Prefs['safe']:
+		url = url + 'safe_mode=on'
+	else:
+		url = url + 'safe_mode=off'
+		
+	return url
